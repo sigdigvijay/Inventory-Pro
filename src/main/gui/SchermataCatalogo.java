6e2cd9a1f5325f398503eb7ad98d24e91e7640f5
Zepa128 <rossi.andrea200439@gmail.com>
2025-12-16 18:51:40 +0100

    Andrea Commit 16/12/25

--- src/main/gui/SchermataCatalogo.java
+++ src/main/gui/SchermataCatalogo.java
@@ -65,7 +65,7 @@ public class SchermataCatalogo extends JPanel {
         List<String[]> products = ProductsDAO.getAllProducts();
         for(String[] p : products) {
             Object[] row = new Object[]{
-                    p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[9] // image path
+                    p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8] // image path
             };
             tableModel.addRow(row);
         }
